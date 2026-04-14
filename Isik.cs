ce68ceb2b9d93907feaf91c4801886f64ac33ed5
opilane <opilane@HP-CZC2349HTB>
2026-04-14 09:19:02 +0300

    Add project files.

---
+++ Isik.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_IvanovNikita
+{
+    public class Isik
+    {
+        //public string Nimi;
+        //public int Vanus;
+
+        public void Tervita()
+        {
+            Console.WriteLine($"Tere! Mina olen {Nimi} ja ma olen {Vanus} aastat vana, olen sündinud {Sünniaasta}.");
+        }
+        // Privaatne väli - otse ligi ei saa
+        private int sünniaasta;
+
+        // Avalik omadus (Property) automaatse get/set logikaga
+        public string Nimi { get; set; }
+
+        // Kontrollitud omadus
+        public int Sünniaasta
+        {
+            get { return sünniaasta; }
+            set
+            {
+                if (value > 1900 && value <= DateTime.Now.Year)
+                    sünniaasta = value;
+                else
+                    Console.WriteLine("Vigane sünniaasta!");
+            }
+        }
+
+        // Arvutatud omadus (ainult lugemiseks / getter)
+        public int Vanus => DateTime.Now.Year - sünniaasta;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using OOP_IvanovNikita;
+
+public class Start()
+{
+    public static void Main(string[] args)
+    {
+        Isik inimene1 = new Isik();
+        inimene1.Nimi = "Mati";
+        inimene1.Sünniaasta = 2000;
+        inimene1.Tervita();
+    }
+}
